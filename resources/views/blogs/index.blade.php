070e542b72cfbbfddad4051260d3d97a031e2100
tamdaz <tamdazohir01@gmail.com>
2023-10-23 23:03:31 +0200

    Fix blog search

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -5,7 +5,7 @@
 @section('container')
 	<h1 class="text-4xl md:text-7xl mt-4 mb-8">Blogs</h1>
 
-	@livewire('article-search', [
+	@livewire('blog-search', [
 		'model' => \App\Models\Blog::class,
 		'thumbName' => 'blog_thumb',
 		'routePrefix' => 'pages.blogs'
