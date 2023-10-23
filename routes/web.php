10a40622e4e53b89e035f1601ca5ea412fade0d0
tamdaz <tamdazohir01@gmail.com>
2023-10-23 17:19:47 +0200

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
-use App\Http\Controllers\PageController;
-use App\Http\Controllers\SitemapController;
-use App\Http\Controllers\Admin\{AdminController, AuthController};
-use App\Http\Controllers\Admin\Resources\{
-	BlogController, ExperienceController, ProfileController, ProjectController, SkillController
-};
+use App\Http\Middleware\AdministratorIP;
+use App\Http\Controllers\{Admin\Resources\CategoryController,
+    PageController,
+    SitemapController,
+    BlogController,
+    Admin\AuthController,
+    Admin\AdminController,
+    Admin\Resources\SkillController,
+    Admin\Resources\ProfileController,
+    Admin\Resources\ExperienceController};
 
 /*
 |--------------------------------------------------------------------------
@@ -23,20 +27,18 @@ use App\Http\Controllers\Admin\Resources\{
 /**
  * This route group is accessible for everyone
  */
-Route::controller(PageController::class)->name('pages.')->group(function () {
-    Route::get('/', 'index')->name('home');
-    Route::get('profile', 'profile')->name('profile');
-    Route::get('skills', 'skills')->name('skills');
-    Route::get('experiences', 'experiences')->name('experiences');
-    Route::get('projects', 'projects')->name('projects');
-    Route::get('projects/{id}', 'project_show')->name('projects.show');
-    Route::get('blogs', 'blogs')->name('blogs');
-    Route::get('blogs/{id}', 'blog_show')->name('blogs.show');
-    Route::get('contact', 'contact')->name('contact');
-    Route::post('contact', 'contact_send')->name('contact_send');
+Route::name('pages.')->group(function () {
+    Route::get('/', [PageController::class, 'index'])->name('home');
+    Route::get('profile', [PageController::class, 'profile'])->name('profile');
+
+    Route::get('blogs', [BlogController::class, 'index'])->name('blogs');
+    Route::get('blogs/{id}', [BlogController::class, 'show'])->name('blogs.show');
+
+    Route::get('contact', [PageController::class, 'contact'])->name('contact');
+    Route::post('contact', [PageController::class, 'contact_send'])->name('contact_send');
 
     // SECRET ROUTE
-    Route::get('components', 'components')->name('components');
+    Route::get('components', [PageController::class, 'components'])->name('components');
 
     Route::get('/sitemap', function () {
         if (!file_exists(public_path('sitemap.xml'))) {
@@ -53,10 +55,7 @@ Route::controller(PageController::class)->name('pages.')->group(function () {
  * This route group is reserved for administrators
  */
 Route::prefix('admin')->name('admin.')->group(function () {
-    /**
-     * Admin Page
-     */
-    Route::middleware('auth')->group(function() {
+    Route::middleware(AdministratorIP::class)->group(function() {
         Route::get('/', [AdminController::class, 'index'])->name('index');
 
         /**
@@ -64,22 +63,15 @@ Route::prefix('admin')->name('admin.')->group(function () {
          */
         Route::resource('skills', SkillController::class)->except(['show']);
         Route::resource('experiences', ExperienceController::class)->except(['show']);
-        Route::resource('projects', ProjectController::class)->except(['show']);
-        Route::resource('blogs', BlogController::class)->except(['show']);
+        Route::resource('blogs', \App\Http\Controllers\Admin\Resources\BlogController::class)
+            ->except(['show']);
+        Route::resource('categories', CategoryController::class)->except(['show']);
+
 
         /**
          * Profile
          */
         Route::get('profile', [ProfileController::class, 'edit'])->name('profile.edit');
         Route::put('profile', [ProfileController::class, 'update'])->name('profile.update');
-    });
-
-    /**
-     * Authentification
-     */
-    Route::prefix('auth')->name('auth.')->group(function () {
-        Route::get('login', [AuthController::class, 'login_page'])->name('login_page');
-        Route::post('login', [AuthController::class, 'login'])->name('login');
-        Route::get('logout', [AuthController::class, 'logout'])->name('logout');
     });
 });
