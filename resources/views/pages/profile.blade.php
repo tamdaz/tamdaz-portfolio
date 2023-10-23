35e512fcc31deced63147f73748f76c012069149
tamdaz <tamdazohir01@gmail.com>
2023-10-23 17:19:22 +0200

    Include skills and experiences in profile.blade.php

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -3,8 +3,19 @@
 @section('title', "Mon profil")
 
 @section('container')
-    <h1 class="text-4xl md:text-7xl mt-4 mb-8">Profil</h1>
+    <h1 class="text-4xl md:text-7xl mt-4 mb-8">A propos de moi</h1>
     <p class="text-justify leading-8 mt-6 text-xl">
         {{ $profile->content }}
     </p>
+    <h1 class="text-2xl md:text-4xl mt-4 mb-8">Mes compétences</h1>
+    <div class="grid grid-cols-4 gap-4">
+        @foreach ($skills as $skill)
+            <x-card-skill
+                    title="{{ $skill['text_primary'] }}"
+                    description="{{ $skill['text_secondary'] }}"
+                    src="{{ $skill['img_skill'] }}" />
+        @endforeach
+    </div>
+    <h1 class="text-2xl md:text-4xl mt-4 mb-8">Expériences professionnelles</h1>
+    <x-timeline :periods="$periods" />
 @endsection
