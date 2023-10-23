1ce7043c0ad763acff3e0ec97dd01fb07545f78e
tamdaz <tamdazohir01@gmail.com>
2023-10-23 17:19:09 +0200

    Update PageController.php

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Http\RedirectResponse;
 use App\Http\Requests\ContactFormRequest;
-use App\Models\{Blog, Experience, Profile, Project, Skill};
+use App\Models\{Experience, Profile, Skill};
 
 class PageController extends Controller
 {
@@ -21,49 +21,9 @@ class PageController extends Controller
     public function profile(): View
     {
         return view('pages.profile', [
-            'profile' => Profile::findOrFail(1)
-        ]);
-    }
-
-    public function skills(): View
-    {
-        return view('pages.skills', [
-            'skills' => Skill::all()
-        ]);
-    }
-
-    public function experiences(): View
-    {
-        return view('pages.experiences', [
+            'profile' => Profile::findOrFail(1),
+            'skills' => Skill::all(),
             'periods' => Experience::all()
-        ]);
-    }
-
-    public function projects(): View
-    {
-        return view('pages.articles', [
-            'type' => "projects"
-        ]);
-    }
-
-    public function project_show(string $id): View
-    {
-        return view('pages.article_show', [
-            'article' => Project::published()->findOrFail($id)
-        ]);
-    }
-
-    public function blogs(): View
-    {
-        return view('pages.articles', [
-            'type' => "blogs"
-        ]);
-    }
-
-    public function blog_show(string $id): View
-    {
-        return view('pages.article_show', [
-            'article' => Blog::published()->findOrFail($id)
         ]);
     }
 
@@ -75,6 +35,7 @@ class PageController extends Controller
     public function contact_send(ContactFormRequest $request): RedirectResponse
     {
         Mail::to('contact@tamdaz.fr')->send(new ContactMail($request));
+
         return redirect()->route('pages.contact')->with('success');
     }
 
