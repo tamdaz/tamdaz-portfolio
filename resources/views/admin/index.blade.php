74412521af7cd8cfddbc56a53aaaec0bb2041b7d
tamdaz <tamdazohir01@gmail.com>
2023-10-25 13:33:50 +0200

    Update alerts in admin home page

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,8 +3,16 @@
 @section('title', "Administration")
 
 @section('content')
-    <x-alert
-        type="debug"
-        primary="Environnement de développement"
-        secondary="{{ $count_blogs }} blogs" />
+    @if(App::isDownForMaintenance())
+        <x-alert
+            type="warn"
+            primary="Site en maintenance"
+            secondary="Pour l'instant, personne ne peut accéder à ce site. La page d'administration reste en revanche accessible pour ceux qui ont le droit d'y accéder." />
+    @endif
+    @if(App::hasDebugModeEnabled() === true)
+        <x-alert
+            type="debug"
+            primary="Environnement de développement"
+            secondary="{{ $count_blogs }} blogs" />
+    @endif
 @endsection
