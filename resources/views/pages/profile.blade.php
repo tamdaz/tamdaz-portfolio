ae14ddd0292610bf3025a4af9b702fc312b7a7bf
tamdaz <tamdazohir01@gmail.com>
2023-12-09 17:17:47 +0100

    Reformat profile page as Markdown

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -4,9 +4,11 @@
 
 @section('container')
     <h1 class="text-4xl md:text-7xl mb-8">A propos de moi</h1>
-    <p class="text-justify leading-8 mt-6 text-xl">
-        {{ $profile->content }}
-    </p>
+    <article class="block w-full mt-2 mb-6 text-justify leading-relaxed text-lg markdown">
+        <p class="text-justify leading-8 mt-6 text-xl">
+            @php echo (new ParsedownExtra())->text($profile['content']) @endphp
+        </p>
+    </article>
     <h1 class="text-2xl md:text-4xl mt-4 mb-8">Mes compétences</h1>
     <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         @foreach ($skills as $skill)
