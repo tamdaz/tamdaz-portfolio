fd686a2c3c102f1369b114da80a0bdf926aa34b9
tamdaz <tamdazohir01@gmail.com>
2023-12-10 15:21:18 +0100

    Delete unused files

--- resources/js/md/markdown-editor.js
+++
@@ -1,7 +0,0 @@
-import "simplemde/dist/simplemde.min.css"
-import * as SimpleMDE from "simplemde/dist/simplemde.min"
-
-const simpleMDE = new SimpleMDE({
-    element: document.getElementById('mdEditor'),
-    showIcons: ['code', 'table']
-})
