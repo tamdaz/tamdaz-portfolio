1cacbfad3b8b6e9bcf58a93fda38f269502bd658
tamdaz <tamdazohir01@gmail.com>
2023-12-13 23:14:11 +0100

    Fix BlogController.php

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Blog;
+use App\Models\Skill;
 use Illuminate\Contracts\View\View;
 
 class BlogController extends Controller
@@ -19,7 +20,7 @@ class BlogController extends Controller
 
     public function show(Blog $blog): View
     {
-        if (! $blog->is_published) {
+        if (!$blog->is_published) {
             abort(404);
         }
 
