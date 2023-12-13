77b9255ece8e35e7c2a3bd50e18f89e9df8371ff
tamdaz <tamdazohir01@gmail.com>
2023-12-13 23:15:19 +0100

    Add new command that allow to delete unrelated attachments (images)

---
+++ app/Console/Commands/AttachmentClearCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Orchid\Attachment\Models\Attachment;
+
+class AttachmentClearCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:attachment-clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clear unused attachments';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        $this->line("Deleting unused attachments...");
+
+        $unrelatedAttachments = (new Attachment())->doesntHave('relationships')->get();
+        $unrelatedAttachments->each->delete();
+
+        $this->info("Successfully deleted unused file/image attachments");
+
+        return Command::SUCCESS;
+    }
+}
