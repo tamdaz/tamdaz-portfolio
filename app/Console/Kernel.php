ef40b7208547d867374f75b7b99ec08574c72e97
tamdaz <tamdazohir01@gmail.com>
2023-12-18 21:27:53 +0100

    Add schedulers

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('app:attachment-clear')->everyOddHour();
+        $schedule->command('app:generate-sitemap')->everyOddHour();
     }
 
     /**
