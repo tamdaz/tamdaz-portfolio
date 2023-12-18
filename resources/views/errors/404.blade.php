471b627f13024ed28b8fd5eaee20ec2c17c41328
tamdaz <tamdazohir01@gmail.com>
2023-12-18 22:28:26 +0100

    Update 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,6 +5,7 @@
 @section('body')
     <div class="w-screen h-screen flex flex-col justify-center items-center">
         <h1 class="animate-zoom text-7xl font-bold">Erreur 404</h1>
-        <span class='my-4 text-xl -skew-x-6 before:content-["\"_"] after:content-["_\""]'>C'est l'écran noir, il n'y a plus d'espoir</span>
+        <p class="my-4">La page que vous essayez de chercher n'existe pas.</p>
+        <x-button route="{{ route('pages.home') }}" type="contained">Retourner à l'accueil</x-button>
     </div>
 @endsection
