4b8bc44bce8bf5065e0210cb33e67ff402a40249
tamdaz <tamdazohir01@gmail.com>
2024-01-10 22:08:28 +0100

    Fix CategoryController.php in API

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -5,14 +5,13 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class CategoryController extends Controller
 {
     public function index(): AnonymousResourceCollection
     {
-        return CategoryResource::collection(Category::all());
+        return CategoryResource::collection(Category::with('blogs')->get());
     }
 
     public function show(Category $category): CategoryResource
