ac2bdc657d7632533e6ffe63c31c175a4331f220
tamdaz <tamdazohir01@gmail.com>
2024-01-14 15:39:07 +0100

    Add dropdown for menus

---
+++ resources/views/components/dropdown.blade.php
@@ -0,0 +1,18 @@
+<div class="inline-block relative" id="menu-{{ $id }}">
+    <span class="cursor-pointer p-8" id="point-to-{{ $id }}">{{ $name }}</span>
+    <nav class="tz-menu hidden" id="dropdown-{{ $id }}">
+        {{ $slot }}
+    </nav>
+</div>
+
+<script>
+    document
+        .querySelector('#menu-{{ $id }}')
+        .addEventListener('mouseenter', () => document.getElementById('dropdown-{{ $id }}').classList.remove('hidden'))
+    ;
+
+    document
+        .querySelector('#menu-{{ $id }}')
+        .addEventListener('mouseleave', () => document.getElementById('dropdown-{{ $id }}').classList.add('hidden'))
+    ;
+</script>
