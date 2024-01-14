46bb700599ffdaafe072fb0075ed1daa1d8a66c2
tamdaz <tamdazohir01@gmail.com>
2024-01-14 01:45:51 +0100

    Update glitch.js

--- resources/js/glitch.js
+++ resources/js/glitch.js
@@ -2,19 +2,19 @@ import { PowerGlitch } from "powerglitch"
 
 PowerGlitch.glitch('#rasberry', {
     "timing": {
-        "duration": 500,
+        "duration": 750,
         "iterations": 1
     },
     "glitchTimeSpan": false,
     "shake": {
-        "amplitudeX": 0.01,
-        "amplitudeY": 0.01
+        "amplitudeX": 0.005,
+        "amplitudeY": 0.005
     },
     "slice": {
         "count": 20,
-        "velocity": 25,
+        "velocity": 50,
         "minHeight": 0.005,
-        "maxHeight": 0.005,
+        "maxHeight": 0.003,
         "hueRotate": false
     }
 })
