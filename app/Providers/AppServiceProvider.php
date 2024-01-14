f2a0fbb98e5919bcc975368c7f1039a5f2c6fa9e
tamdaz <tamdazohir01@gmail.com>
2024-01-14 13:27:54 +0100

    Delete Telescope in AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,10 +17,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->environment('local')) {
-            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
-            $this->app->register(TelescopeServiceProvider::class);
-        }
+        // ...
     }
 
     /**
