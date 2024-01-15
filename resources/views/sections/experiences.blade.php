d2b5846a029b49ff0eab21eea7ee143cf8df1801
tamdaz <tamdazohir01@gmail.com>
2024-01-15 23:00:00 +0100

    Fix text align on experiences

--- resources/views/sections/experiences.blade.php
+++ resources/views/sections/experiences.blade.php
@@ -1,12 +1,12 @@
 <section>
-	<div class="w-full h-full lg:px-16 justify-center text-center mt-16">
+	<div class="w-full h-full lg:px-16 mt-16">
 		<div class="grid grid-cols-2 w-full lg:w-[1000px] px-[16px] m-auto">
 			<div>
-				<h2 class="text-2xl font-bold">Mes expériences professionnelles</h2>
+				<h2 class="text-2xl text-center">Mes expériences professionnelles</h2>
 				<x-timeline :periods="$timelines['experience']" />
 			</div>
 			<div>
-				<h2 class="text-2xl font-bold">Mes formations</h2>
+				<h2 class="text-2xl text-center">Mes formations</h2>
 				<x-timeline :periods="$timelines['formation']" />
 			</div>
 		</div>
