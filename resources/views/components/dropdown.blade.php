3e280073f4b2b22156d70a65f753752c713c9189
tamdaz <tamdazohir01@gmail.com>
2024-01-15 21:56:09 +0100

    Optimize dropdown.blade.php

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,6 +1,6 @@
 <div class="inline-block relative" id="menu-{{ $id }}">
     <span class="cursor-pointer p-8" id="point-to-{{ $id }}">{{ $name }}</span>
-    <nav class="tz-menu hidden" id="dropdown-{{ $id }}">
+    <nav class="tz-menu hidden animate-dropdown" id="dropdown-{{ $id }}">
         {{ $slot }}
     </nav>
 </div>
