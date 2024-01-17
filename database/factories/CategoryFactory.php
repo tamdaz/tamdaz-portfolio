1850a8b4671ef8ce7bafd40b2413a30608ac930e
tamdaz <tamdazohir01@gmail.com>
2024-01-17 21:19:21 +0100

    Update CategoryFactory.php

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -18,7 +18,7 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->word(),
+            'name' => fake()->realText(10),
         ];
     }
 }
