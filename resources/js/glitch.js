e6dc40b413d481d43b177e594ddeb8fa289beb6b
tamdaz <tamdazohir01@gmail.com>
2024-01-17 21:20:58 +0100

    Fix glitch.js

--- resources/js/glitch.js
+++ resources/js/glitch.js
@@ -1,21 +1,25 @@
 import { PowerGlitch } from "powerglitch"
 
-PowerGlitch.glitch('#rasberry', {
-    "timing": {
-        "duration": 1000,
-        "iterations": 1,
-        "easing": "ease-out"
-    },
-    "glitchTimeSpan": false,
-    "shake": {
-        "amplitudeX": 0.004,
-        "amplitudeY": 0.004
-    },
-    "slice": {
-        "count": 10,
-        "velocity": 20,
-        "minHeight": 0.005,
-        "maxHeight": 0.004,
-        "hueRotate": true
-    }
-})
+window.onload = async () => {
+    await document.querySelector('#rasberry_img').classList.add('animate-glitch');
+    await document.querySelector('#presentation').classList.add('animate-zoom-avatar');
+
+    PowerGlitch.glitch('#rasberry', {
+        "timing": {
+            "duration": 1000,
+            "iterations": 1
+        },
+        "glitchTimeSpan": false,
+        "shake": {
+            "amplitudeX": 0.004,
+            "amplitudeY": 0.004
+        },
+        "slice": {
+            "count": window.innerWidth > 600 ? 10 : 5,
+            "velocity": window.innerWidth > 600 ? 60 : 30,
+            "minHeight": 5 / 1000,
+            "maxHeight": 6 / 1000,
+            "hueRotate": true
+        }
+    })
+}
