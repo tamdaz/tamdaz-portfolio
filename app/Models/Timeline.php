f6e1969d4a7b38f64f0a9de1d9675a42740bb99e
tamdaz <tamdazohir01@gmail.com>
2024-01-20 12:24:50 +0100

    Improve Timeline.php

--- app/Models/Timeline.php
+++ app/Models/Timeline.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Carbon\Carbon;
 use DateTimeInterface;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Orchid\Filters\Filterable;
@@ -16,16 +18,35 @@ class Timeline extends Model
 {
     use AsSource, Filterable, HasFactory;
 
+    /**
+     * @var string[]
+     */
     protected $fillable = [
         'date_start', 'date_end', 'description', 'type',
     ];
 
+    /**
+     * @var array<string, string>
+     */
     protected $casts = [
         'date_start' => 'date',
         'date_end' => 'date',
         'description' => 'string',
         'type' => 'string',
     ];
+
+    /**
+     * Filter timeline by type
+     *
+     * @param  Builder<Timeline>  $builder
+     * @return Collection<int, Timeline>
+     */
+    public function scopeFilterByType(Builder $builder, string $type): Collection
+    {
+        return $builder->where('type', '=', $type)
+            ->latest('date_start')
+            ->get();
+    }
 
     protected function serializeDate(DateTimeInterface $date): string
     {
