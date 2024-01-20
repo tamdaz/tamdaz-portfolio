3c9dcea7779377d147c84d679c0b85f384ecaeb3
tamdaz <tamdazohir01@gmail.com>
2024-01-20 12:22:10 +0100

    Fix theme.js

--- resources/js/theme.js
+++ resources/js/theme.js
@@ -1,6 +1,15 @@
 let changeThemeHeader = document.getElementById('changeThemeHeader');
 let changeThemeFooter = document.getElementById('changeThemeFooter');
 
+if (!localStorage.getItem('theme')) {
+    localStorage.setItem('theme', 'light');
+
+    changeThemeHeader.checked = false;
+    changeThemeFooter.checked = false;
+
+    document.documentElement.classList.remove("dark")
+}
+
 function changeTheme() {
     if (localStorage.getItem('theme') === "light") {
         localStorage.setItem('theme', 'dark')
