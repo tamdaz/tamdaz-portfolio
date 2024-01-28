f77344f0b45bfdf4a97cf6898c67f9f5ace4f8f8
tamdaz <tamdazohir01@gmail.com>
2024-01-28 17:34:03 +0100

    Format PlatformScreen.php

--- app/Orchid/Screens/PlatformScreen.php
+++ app/Orchid/Screens/PlatformScreen.php
@@ -53,7 +53,7 @@ class PlatformScreen extends Screen
     public function layout(): iterable
     {
         return [
-            Layout::view('platform::partials.update-assets')
+            Layout::view('platform::partials.update-assets'),
         ];
     }
 }
