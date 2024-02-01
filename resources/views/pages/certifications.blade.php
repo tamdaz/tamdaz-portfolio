69ea8345c68cd7aaddc6ba0fc42178affc8a0635
tamdaz <tamdazohir01@gmail.com>
2024-02-01 18:38:40 +0100

    Add explanation for certifications.blade.php

--- resources/views/pages/certifications.blade.php
+++ resources/views/pages/certifications.blade.php
@@ -5,7 +5,8 @@
 @section('container')
 	<h1 class="text-4xl md:text-7xl font-bold animate-title-anim">Certifications</h1>
 	<p class="mt-4 mb-8">
-		Voici l'ensemble de certifications que j'ai obtenu.
+		Les parcours de certifications complémentaires <i>(PCC)</i> permettent de valider l'acquisition
+		des compétences supplémentaires.
 	</p>
 	<div class="gap-4">
 		@forelse($certifications as $certification)
