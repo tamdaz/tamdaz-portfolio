7d96f96ec2cfed2bea64f12b66e471da986ee2d8
tamdaz <tamdazohir01@gmail.com>
2024-02-17 22:19:17 +0100

    Format reports.blade.php

--- resources/views/pages/reports.blade.php
+++ resources/views/pages/reports.blade.php
@@ -11,8 +11,10 @@
 	<p class="mb-6">
 		Les comptes-rendus sont publiés, mis à jour et accessibles pour tout lecteur.
 		Elles sont triées par date de création du plus récent au plus ancien.
-		Également, elles sont catégorisées par les 3 blocs en lien avec le BTS SIO: Bloc 1 <i>(Mise à disposition des
-		services informatiques)</i>, Bloc 2 <i>(SLAM)</i> et Bloc 3 <i>(Cybersécurité)</i>.
+		Également, elles sont catégorisées par les 3 blocs en lien avec le BTS SIO :
+		Bloc 1 <i>(Mise à disposition des services informatiques)</i>,
+		Bloc 2 <i>(SLAM)</i> et
+		Bloc 3 <i>(Cybersécurité)</i>.
 	</p>
 
 	<x-alert
@@ -21,5 +23,6 @@
 		n'hésitez pas à me prévenir en envoyant un message dans la page de contact." />
 
 	@livewire('report-search')
+
 	@livewireScripts
 @endsection
